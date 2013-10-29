19cd2e25c8694e2bbd65f2ca83f97f50d6bbc96e
Nhan Doan <nhan.doanngoc@asnet.com.vn>
2013-10-29 08:20:09 +0700

    Add component and libraries Symfony 2

---
+++ symfony/index.php
@@ -0,0 +1,12 @@
+<?php
+  require_once 'vendor/autoload.php';
+  use Symfony\Component\HttpFoundation\File;
+  // use Symfony\Component\HttpFoundation\Response;
+
+  // $request = Request::createFromGlobals();
+
+  // $uri = $request->getPathInfo();
+  echo "here";
+
+
+?>
