56779d6eaf5caabb4bb2b4d2d02267aac3a35515
Nhan Doan <nhan.doanngoc@asnet.com.vn>
2014-02-10 17:54:59 +0700

    - Delete all file

--- app-store/api_remote/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('remoteAPI.php');
-	$apiObj = new RemoteAPI('http://localstore.fitmoo.com/', '/?q=my_service');
-	$ret = $apiObj->view_index();
-	echo json_encode($ret);	
-?>
-
-	
-
