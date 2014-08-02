1de5bef8b72784d311588495a68a24cef601f564
Trung Tran <trung.tranngoc@asnet.com.vn>
2014-08-02 11:47:15 +0700

    Refactor signup / login form

--- frontend/app/scripts/auth/controllers/sign-up-ctrl.js
+++ frontend/app/scripts/auth/controllers/sign-up-ctrl.js
@@ -8,18 +8,35 @@ angular
 		function ($scope, $modal) {
 
 ///////////////////////////////////////////////////////////////////////////////////////
+	var _isSignUp = false,
+			modalSignUp = function() {
+				$modal.open({
+					templateUrl: 'views/directives/modals/sign-up.html',
+					windowClass: 'login-register',
+					controller: signUpModalInstanceCtrl
+				});
+			};
 
-	$scope.open = function (size) {
-
-		var modalSignUp = $modal.open({
-			templateUrl: 'views/directives/modals/sign-up.html',
-			windowClass: 'login-register',
-			controller: function() {
-				
-			},
-			size: size
-		});
+	$scope.open = function (isShow) {
+		_isSignUp = isShow;
+		modalSignUp();
 	};
+
+	var signUpModalInstanceCtrl = [
+			'$scope',
+			'$modalInstance',
+			'$timeout',
+		 	function ($scope, $modalInstance, $timeout) {
+				$scope.isSignUp = _isSignUp;
+
+				$scope.toggleForm = function (isShow) {
+					$timeout(function() {
+						$scope.isSignUp = isShow;
+					}, 100);
+				}
+
+			}
+		];
 
 ///////////////////////////////////////////////////////////////////////////////////////
 		}
