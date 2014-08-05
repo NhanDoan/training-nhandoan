404bfd6a0dc275dc73e292c757a28fafc1c77cca
Tuong Le <tuong.le@asnet.com.vn>
2014-08-05 11:08:26 +0700

    developed for API user

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,19 +1,25 @@
 class SessionsController < ApplicationController
+	@result = nil
+
 	def create
 		if user = User.authenticate(params[:email], params[:password])
 			session[:user_id] = user.id
 			# redirect_to root_path, :notice => "Logged in successfully"
-			format.json { user: user, message: "Logged in successfully" }
+			@result = { user: user, message: "Logged in successfully" }
 		else
 			# flash.now[:alert] = "Invalid login/password combination"
 			# render :action => 'new'
-			format.json { user: nil, message: "Invalid login/password combination" }
+			
+			@result = { user: nil, message: "Invalid login/password combination" }
 		end
+
+		format.json { render json: @result }
 	end
 
 	def destroy
 		reset_session
 		# redirect_to root_path, :notice => "You successfully logged out"
-		format.json { ok: '0', message: "You successfully logged out" }
+		@result = { ok: '0', message: "You successfully logged out" }
+		format.json { render json: @result }
 	end
 end
